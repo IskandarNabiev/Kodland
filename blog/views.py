c50fd244caea706325dbfaa38e050303ca976893
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-05-04 09:56:29 +0300

    For circle

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.shortcuts import render
 from django.views import View
 
@@ -9,7 +10,10 @@ def home(request):
     context = {'posts': posts}
     return render(request, 'blog/home.html', context)
 
-    
+def last(request):
+    last_posts = Post.objects.filter(date_posted)[:1][::-1]
+    context = {'last_posts': last_posts}
+    return render(request, 'blog/home.html', context)
 
 class PostCreate(View):
     def get(self, request):
